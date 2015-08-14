ba556fc3ee84ce57e9e631c05d59411f6c661dda
Michael Arnott <arnottmj@aol.com>
2015-08-14 11:24:40 +0100

    setup hellow world

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
   def index
-    Hello World!
+    
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: "songs#index"
+
   resources :songs
 
   # The priority is based upon order of creation: first created -> highest priority.
