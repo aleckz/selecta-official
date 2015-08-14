ed1ace40c0a8f7e3ba3a9777bb37442b59196247
Alex <aleksandernasr@gmail.com>
2015-08-14 11:54:41 +0100

    created users controller, user model and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'users/new'
+
   root to: "songs#index"
 
   resources :songs
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814103253) do
+ActiveRecord::Schema.define(version: 20150814104953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "songs", force: :cascade do |t|
     t.integer  "soundcloud_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
