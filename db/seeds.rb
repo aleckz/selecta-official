700b2eece921e4750dab6e7db377353617774dff
Salman <salman.karim@hotmail.com>
2015-08-17 11:24:44 +0100

    includes algorithm for next song

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(email: 'salman@selecta.com', password: '123')
+User.create(email: 'michael@selecta.com', password: '123')
+User.create(email: 'alex@selecta.com', password: '123')
+User.create(email: 'faisal@selecta.com', password: '123')
+
+Song.create(soundcloud_id: 1)
+Song.create(soundcloud_id: 2)
+Song.create(soundcloud_id: 3)
+Song.create(soundcloud_id: 4)
+Song.create(soundcloud_id: 5)
+Song.create(soundcloud_id: 6)
+Song.create(soundcloud_id: 7)
+Song.create(soundcloud_id: 8)
+Song.create(soundcloud_id: 9)
+Song.create(soundcloud_id: 10)
+
+User.all.each {|user| user.songs << Song.find(1)}
+User.find(1).songs << Song.find(2)
+User.find(1).songs << Song.find(3)
+User.find(1).songs << Song.find(4)
+User.find(1).songs << Song.find(5)
+
+User.find(2,3,4).each {|user| user.songs << Song.find(6)}
+User.find(2).songs << Song.find(8)
+User.find(2).songs << Song.find(9)
+User.find(2).songs << Song.find(4)
+
+User.find(3).songs << Song.find(7)
+User.find(3).songs << Song.find(4)
+User.find(3).songs << Song.find(10)
+
+User.find(4).songs << Song.find(8)
