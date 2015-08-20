e8997e975ff26564966d15ba72f1168e7cba1a9f
Alex <aleksandernasr@gmail.com>
2015-08-20 11:37:16 +0100

    fixed bug where songs array was not removing previous search results and displaying all

--- app/assets/javascripts/controllers/songSearchController.js
+++ app/assets/javascripts/controllers/songSearchController.js
@@ -9,12 +9,14 @@ selecta.controller("SongSearchController", ['$scope', '$resource', function($sco
   $scope.doSearch = function(){
     if ($scope.searchTerm !== '') {
       return SC.get('https://api.soundcloud.com/tracks', { q: $scope.searchTerm }, function(tracks) {
+        temporarySearchList = [];
         for (var i=0; i<tracks.length; i++) {
           if ((tracks[i].streamable) === true) {
             console.log("streamable true");
-            $scope.songs.push(tracks[i]);
+            temporarySearchList.push(tracks[i]);
           }
         }
+        $scope.songs = temporarySearchList;
         $scope.$apply();
       });
     }
